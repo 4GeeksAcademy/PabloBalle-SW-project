7a898bf5f55bb36333cf02e2e5c3e9f73f817de3
Pablo Ballesteros Suarez <132361283+PabloBalle@users.noreply.github.com>
2023-07-30 21:10:00 +0000

    V1.0

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
+import { Home } from "./views/Home.jsx";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { Users } from "./views/Users.jsx";
+import { UserDetails } from "./views/UserDetails.jsx";
+import { Posts } from "./views/Posts.jsx";
+
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Navbar } from "./component/Navbar.jsx";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -23,6 +27,9 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
+						<Route path="/users" element={<Users/>} />
+						<Route path="/users/:userId" element={<UserDetails/>}/>
+						<Route path="/posts" element={<Posts/>}/>
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
